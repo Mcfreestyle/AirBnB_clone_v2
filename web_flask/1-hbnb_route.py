6eb27798eff63b743db82f775d41245f12c4cb2e
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-05-14 00:27:57 -0500

    feat: starts a flask app with a URL

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module provides `hello` and `hbnb` functions"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Show a message"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Show a message"""
+    return ("HBNB")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
