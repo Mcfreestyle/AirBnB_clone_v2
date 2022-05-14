51c28a328f681c75e87aa9e97247a7ee0a16ead9
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-05-14 16:08:19 -0500

    feat: starts a flask app displaying a HTML page with a passed number in the url

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This module provides `hello`, `hbnb`, `c`, `python`,
+   `number`, `number_template` functions"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Show a message"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Show a message"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Show a message with a passed parameter in the url"""
+    text = text.replace('_', ' ')
+    return ("C " + text)
+
+
+@app.route("/python/", strict_slashes=False, defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """Show a message with a passed parameter in the url"""
+    text = text.replace('_', ' ')
+    return ("Python " + text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Show a message with a passed parameter(number) in the url"""
+    return ("{} is a number".format(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Display a HTML page with a passed number in the url"""
+    return (render_template("5-number.html", n=n))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
