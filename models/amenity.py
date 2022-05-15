8cbdf5392c694195bb7d62eb8bbe7dba5b1b6ba0
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-05-15 10:53:05 -0500

    style: deleted innecesary importation

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 from os import getenv
 
 
@@ -14,7 +13,7 @@ class Amenity(BaseModel, Base):
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
                                         'Place',
-                                        secondary=place_amenity,
+                                        secondary='place_amenity',
                                         back_populates="amenities")
     else:
         name = ''
